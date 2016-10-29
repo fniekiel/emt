941b010d2dc71a2c17b04ccaaf05a46c003dec57
Florian Niekiel <florian.niekiel@niemail.org>
2016-10-29 10:13:12 -0700

    added io module docstring

--- emt/io/__init__.py
+++ emt/io/__init__.py
@@ -1,1 +1,8 @@
+'''
+The io module provides tools to read various fileformats typically occurring in electron microscopy.
+
+These are converted to the EMD file format for processing and interchangeability. 
+The different readers are organized into submodules.
+'''
+
 import ser
