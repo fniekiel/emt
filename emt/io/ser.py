1a63bd46b2d58898322b812b2486896f55fa89d4
Florian Niekiel <florian.niekiel@niemail.org>
2016-10-29 09:57:56 -0700

    setting up the io module

---
+++ emt/io/ser.py
@@ -0,0 +1,9 @@
+'''
+This module provides an interface to the SER file format written by TIA.
+
+Following the information provided by Dr Chris Boothroyd (http://www.er-c.org/cbb/info/TIAformat/)
+'''
+
+
+def test_ser():
+    print('test_ser')
