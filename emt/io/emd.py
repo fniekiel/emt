e4cb308964ebf9ae0926aea65e40a6893453279c
Florian Niekiel <florian.niekiel@niemail.org>
2016-11-03 10:29:50 -0700

    (EMD) added file opening/closing

--- emt/io/emd.py
+++ emt/io/emd.py
@@ -13,8 +13,39 @@ class fileEMD:
     Class to represent EMD files.
     '''
     
-    def __init__(self):
-        pass
+    def __init__(self, filename, readonly=False, verbose=False):
+        '''Init opening/creating the file.
+        
+        input:
+        - filename (string)     name of the EMD file
+        - readonly (bool)       set to open in read only mode
+        - verbose (bool)        set to get extensive output while reading the file
+        '''
+        
+        # necessary declarations in case something goes bad
+        self.file_hdl = None
+        
+        # check for string
+        if not isinstance(filename, str):
+            raise TypeError('Filename is supposed to be a string')
+
+        # try opening the file
+        if readonly:
+            try:
+                self.file_hdl = h5py.File(filename, 'r')
+            except:
+                print('Error opening file for readonly: "{}"'.format(filename))
+                raise
+        else:
+            try:
+                self.file_hdl = h5py.File(filename, 'a')
+            except:
+                print('Error opening file for read/write: "{}"'.format(filename))
+
+
         
     def __del__(self):
-        pass
+        '''Destructor for EMD file object'''
+        # close the file
+        if(self.file_hdl):
+            self.file_hdl.close()
--- emt/tests/test_io_emd.py
+++ emt/tests/test_io_emd.py
@@ -10,9 +10,29 @@ class test_emd(unittest.TestCase):
     Test the EMD io module
     '''
     
-    def test_open_emd(self):
-        pass
+    def test_init_emd(self):
         
+        # wrong argument type
+        with self.assertRaises(TypeError):
+            femd = emt.io.emd.fileEMD(42)
+        
+        # non existing file in readonly
+        with self.assertRaises(IOError):
+            femd = emt.io.emd.fileEMD('resources/output/doesnotexist.emd', readonly=True)
+            
+        # impossible file for read/write
+        with self.assertRaises(IOError):
+            femd = emt.io.emd.fileEMD('resources/output/output/')
+            
+        
+            
+        # open existing file
+        femd = emt.io.emd.fileEMD('resources/Au_SAED_D910mm_20x_at_800/Au_SAED_D910mm_20x_at_800.emd')
+        # open existing file readonly
+        femd = emt.io.emd.fileEMD('resources/Au_SAED_D910mm_20x_at_800/Au_SAED_D910mm_20x_at_800.emd', readonly=True)
+        # open nonexisting file for writing
+        femd = emt.io.emd.fileEMD('resources/output/test.emd')
+
 
 # to test with unittest runner
 if __name__ == '__main__':
