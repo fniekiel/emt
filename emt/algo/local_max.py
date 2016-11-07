bf8afcf02323fbf6f07b1945c836db92e1706be3
Florian Niekiel <florian.niekiel@niemail.org>
2016-11-07 11:31:47 -0800

    (radial_profile) laying out modules

---
+++ emt/algo/local_max.py
@@ -0,0 +1,23 @@
+'''
+Find local maxima in an image.
+'''
+
+
+def local_max():
+    '''
+    Find local maxima from comparing dilated and eroded images.
+    
+    Calculates images with maximum and minimum within given radius. If the difference is larger than the threshold, the original pixel position with max value is detected as local maximum.
+    
+    input:
+    - img
+    - r
+    - thresh
+    
+    return:
+    - pos
+    ''''
+    
+    pos = []
+    
+    return pos
---
+++ emt/eva/ring_diff.py
@@ -0,0 +1,31 @@
+'''
+Module to process ring diffraction patterns.
+'''
+
+import emt.algo.local_max
+import emt.io.emd
+
+def evaSinglePattern():
+    '''
+    Evaluate a single ring diffraction pattern.
+    
+    input:
+    return:
+    '''
+    
+    
+    return None
+    
+    
+
+def evaEMDFile():
+    '''
+    Run on a single EMD file.
+    
+    input:
+    return:
+    '''
+    
+    evaSinglePattern()
+    
+    return None
